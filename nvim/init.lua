7986b19ca8d3d1c6478aaecd3edb35d2d354ab9c
Derrik <heyimderrik@gmail.com>
2025-03-30 08:18:47 -0400

    moving theming to its own file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,5 @@
 require("derrik.vim-config")
 require("derrik.lazy")
+require("derrik.custom-theming")
 
-vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#5eff6c" })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "white" })
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#5ef1ff" })
 
---
+++ nvim/lua/derrik/custom-theming.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_set_hl(0, "Spellhl", { fg = "#F47A4A" })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#5eff6c" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "white" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#5ef1ff" })
+
+
