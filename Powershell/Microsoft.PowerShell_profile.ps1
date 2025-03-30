7693b0beff145ea36f1b548d22adfbce3fe715f5
Derrik <heyimderrik@gmail.com>
2025-03-30 08:17:39 -0400

    powershell alias

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,8 @@
+Write-Output "test"
+Function GoToDotFiles { Set-Location -Path "~/.dotfiles/" }
+Function GoToProjectFiles { Set-Location -Path "~/.projects/" }
+Set-Alias -Name dot -Value GoToDotFiles
 
-
+Set-Alias -Name proj -Value GoToProjectFiles
 
 
