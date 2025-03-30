1753e3eb743672de248fae01f014b5ecce310771
Derrik <heyimderrik@gmail.com>
2025-03-30 03:35:01 -0400

    term title to match cwd folder name

--- nvim/lua/derrik/vim-config.lua
+++ nvim/lua/derrik/vim-config.lua
@@ -4,17 +4,16 @@
 vim.g.mapleader = " "
 vim.api.nvim_set_option("clipboard", "unnamed")
 
-			
- 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.statuscolumn = "%s %l %r"
 vim.opt.numberwidth = 7
 vim.o.wrap = false
 
+vim.o.title = true
+vim.o.titlestring = "%{fnamemodify(getcwd(), ':t')}"
 
 vim.o.smartcase = true
-
 
 vim.keymap.set("n", "<S-TAB>", ":bprevious<cr>")
 
@@ -34,7 +33,12 @@ vim.keymap.set("n", "<leader>se", function()
 	vim.diagnostic.open_float()
 end, { desc = "Show line error" })
 
-vim.opt.spell= true
+vim.opt.spell = true
 
 -- Toggle spell checking
-vim.api.nvim_set_keymap('n', '<leader>sp', ':set spell!<CR>', { noremap = true, silent = true , desc = "Spell check toggle" })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>sp",
+	":set spell!<CR>",
+	{ noremap = true, silent = true, desc = "Spell check toggle" }
+)
