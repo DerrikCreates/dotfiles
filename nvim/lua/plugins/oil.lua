e3cdd96e6b401a2b094829b06a6ea9af2786c09e
Derrik <heyimderrik@gmail.com>
2025-09-28 03:04:37 -0400

    use lazy keys field

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,16 +1,11 @@
-return{
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = 
-  {
-  },
-
-  config = function()
-    vim.keymap.set("n","<leader>f","<cmd>Oil<cr>")
-
-    require("oil").setup(opts)
-  end,
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  lazy = false,
+return {
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	keys = {
+		{ "<leader>f", "<cmd>Oil<cr>", mode = "n" },
+	},
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	lazy = false,
 }
